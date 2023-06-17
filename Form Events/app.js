9311629d19368ae748af3ea76518b117c775bfcf
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-06-17 17:43:01 +0530

    just made an object form data and collected all the data from the form

--- Form Events/app.js
+++ Form Events/app.js
@@ -6,18 +6,25 @@ const veggieSelector = document.querySelector('#veggie');
 
 const formData={};
 
-creditCard.addEventListener('input',e=>{
+// creditCard.addEventListener('input',e=>{
     
-    console.log('cc changed',e);
-    formData['cc']=e.target.value;
-});
-veggieSelector.addEventListener('input',e=>{
+//     console.log('cc changed',e);
+//     formData['cc']=e.target.value;
+// });
+// veggieSelector.addEventListener('input',e=>{
     
-    console.log('veggie changed',e);
-    formData['veggie']=e.target.value;
-});
-termsCheckBox.addEventListener('input',e=>{
+//     console.log('veggie changed',e);
+//     formData['veggie']=e.target.value;
+// });
+// termsCheckBox.addEventListener('input',e=>{
     
-    console.log('terms changed',e);
-    formData['terms']=e.target.checked;
-});
+//     console.log('terms changed',e);
+//     formData['terms']=e.target.checked;
+// });
+
+for(let input of [creditCard,termsCheckBox,veggieSelector]){
+    input.addEventListener('input',({target}) =>{
+        const {name,type,value,checked}=target;
+        formData[name] = type ==='checkbox'?checked:value;
+    });
+}
